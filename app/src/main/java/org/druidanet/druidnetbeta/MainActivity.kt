75dff32bcf93dae2f8a7cede01a4e9d7852719a7
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2024-08-02 00:34:43 +0200

    Added welcome text and catalog button

--- app/src/main/java/org/druidanet/druidnetbeta/MainActivity.kt
+++ app/src/main/java/org/druidanet/druidnetbeta/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowColumn
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.druidanet.druidnetbeta.ui.theme.DruidNetBetaTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +33,10 @@ class MainActivity : ComponentActivity() {
             DruidNetBetaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                            .wrapContentSize(Alignment.Center)
                     )
                 }
             }
@@ -31,17 +45,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            Text(stringResource(R.string.welcome))
+            Spacer(modifier = Modifier.height(48.dp))
+            Button(onClick = {}) {
+                Text(stringResource(R.string.menu_action_catalog))
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DruidNetBetaTheme {
-        Greeting("Android")
+        Greeting(modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center))
     }
 }
