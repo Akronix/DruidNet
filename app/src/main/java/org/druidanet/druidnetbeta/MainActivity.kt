f9e2ddc7bcff67b51aa2335abb514d5a9308d465
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2024-08-02 01:44:55 +0200

    Changed app icon

--- app/src/main/java/org/druidanet/druidnetbeta/MainActivity.kt
+++ app/src/main/java/org/druidanet/druidnetbeta/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,6 +53,10 @@ fun Greeting(modifier: Modifier = Modifier) {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceEvenly,
         ) {
+            Image(
+                painter = painterResource(R.drawable.app_icon),
+                contentDescription = "App logo")
+            Spacer(modifier = Modifier.height(24.dp))
             Text(stringResource(R.string.welcome))
             Spacer(modifier = Modifier.height(48.dp))
             Button(onClick = {}) {
