197186a649f43013f9f49811f3a01d908f8856d1
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2024-08-06 13:53:29 +0200

    Added TopBar to Catalog

--- app/src/main/java/org/druidanet/druidnetbeta/CatalogActivity.kt
+++ app/src/main/java/org/druidanet/druidnetbeta/CatalogActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package org.druidanet.druidnetbeta
 
 import android.graphics.fonts.FontStyle
@@ -7,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
@@ -16,19 +19,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle.Companion.Italic
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,79 +53,120 @@ class CatalogActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DruidNetBetaTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Catalog(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CatalogAndBar()
             }
         }
     }
-}
 
-@Composable
-fun PlantCard(plant: Plant, modifier: Modifier) {
-    Card(modifier = modifier) {
-        Column{
-            Image(
-                painter = painterResource(plant.imageResourceId),
-                contentDescription = "Image for {plant.latinName}",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(194.dp),
-                contentScale = ContentScale.Crop
-            )
-            Text(
-                text = plant.latinName,
-                modifier = Modifier.padding(16.dp),
-                fontStyle = Italic,
-                style = MaterialTheme.typography.headlineSmall,
-                fontWeight = FontWeight.Bold
-            )
+    @Composable
+    fun PlantCard(plant: Plant, modifier: Modifier) {
+        Card(modifier = modifier) {
+            Column {
+                Image(
+                    painter = painterResource(plant.imageResourceId),
+                    contentDescription = "Image for {plant.latinName}",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(194.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = plant.latinName,
+                    modifier = Modifier.padding(16.dp),
+                    fontStyle = Italic,
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold
+                )
 
+            }
         }
     }
-}
 
-@Composable
-fun PlantsList(plantsList: List<Plant>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(plantsList) { plant ->
-            PlantCard(
-                plant = plant,
-                modifier = Modifier.padding(8.dp)
-            )
+    @Composable
+    fun PlantsList(plantsList: List<Plant>, modifier: Modifier = Modifier) {
+        LazyColumn(modifier = modifier) {
+            items(plantsList) { plant ->
+                PlantCard(
+                    plant = plant,
+                    modifier = Modifier.padding(8.dp)
+                )
+
+            }
 
         }
 
     }
 
-}
-
-@Composable
-fun Catalog(modifier: Modifier = Modifier) {
-    val layoutDirection = LocalLayoutDirection.current
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .statusBarsPadding()
-            .padding(
-                start = WindowInsets.safeDrawing.asPaddingValues()
-                    .calculateStartPadding(layoutDirection),
-                end = WindowInsets.safeDrawing.asPaddingValues()
-                    .calculateEndPadding(layoutDirection),
-            ),
-    ) {
-        PlantsList(
-            plantsList = PlantsDataSource().loadPlants(),
+    @Composable
+    fun CatalogAppBar(modifier: Modifier = Modifier) {
+        CenterAlignedTopAppBar(
+            title = {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .size(dimensionResource(id = R.dimen.bar_image_size))
+                            .padding(dimensionResource(id = R.dimen.padding_small)),
+                        painter = painterResource(R.drawable.menu_book),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = stringResource(R.string.catalog_bar_title),
+                        style = MaterialTheme.typography.displaySmall,
+                        modifier = Modifier.padding(start = 6.dp)
+                    )
+                }
+            },
+            modifier = modifier
         )
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    DruidNetBetaTheme {
-        Catalog()
+    @Composable
+    fun Catalog(modifier: Modifier = Modifier) {
+        Surface(
+            modifier = modifier
+        ) {
+            PlantsList(
+                plantsList = PlantsDataSource().loadPlants()
+            )
+        }
     }
+
+
+    @Composable
+    fun CatalogAndBar() {
+        Scaffold(
+            topBar = {
+                CatalogAppBar()
+            }
+        ) { innerPadding ->
+            Catalog(
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+
+    /**
+     * Composable that displays what the UI of the app looks like in light theme in the design tab.
+     */
+    @Preview(showBackground = true)
+    @Composable
+    fun CatalogPreview() {
+        DruidNetBetaTheme {
+            CatalogAndBar()
+        }
+    }
+
+    /**
+     * Composable that displays what the UI of the app looks like in dark theme in the design tab.
+     */
+//    @Preview
+//    @Composable
+//    fun CatalogDarkThemePreview() {
+//        DruidNetBetaTheme(darkTheme = true) {
+//            CatalogAndBar()
+//        }
+//    }
+
 }
