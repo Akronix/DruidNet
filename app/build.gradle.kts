390bfde1223404e09b362da1ed54bf19392da954
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2024-10-14 20:52:13 +0200

    Release prototype 1 for beta closing testing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "org.druidanet.druidnet"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -28,6 +28,11 @@ android {
 
     buildTypes {
         release {
+
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
+
             isDebuggable = false
 
             // Enables code shrinking, obfuscation, and optimization for only
