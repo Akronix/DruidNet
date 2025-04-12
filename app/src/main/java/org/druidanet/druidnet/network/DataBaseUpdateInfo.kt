1dcbc22d1dce5ff7970986cf7decf6b5537d891d
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2025-04-12 09:14:05 +0200

    Remove useless hasChanged booleans vars in dbinfo.json backend file

--- app/src/main/java/org/druidanet/druidnet/network/DataBaseUpdateInfo.kt
+++ app/src/main/java/org/druidanet/druidnet/network/DataBaseUpdateInfo.kt
@@ -5,7 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class DataBaseUpdateInfo (
     val versionDB: Long,
-    val plantsChanged: Boolean,
-    val biblioChanged: Boolean,
     val images: List<String>
 )
