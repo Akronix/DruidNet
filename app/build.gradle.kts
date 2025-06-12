93bd2bfc6b8111e07494d857c5280f0b401f9740
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2025-06-12 17:18:03 +0200

    Release version 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "org.druidanet.druidnet"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.2.2"
+        versionCode = 15
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
