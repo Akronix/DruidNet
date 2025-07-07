f3ec3aa8e13a55d84460e2d9e9a1234ba3c1d674
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2025-07-07 13:51:32 +0200

    Add temporary text to Glossary screen
    This commit adds a temporary text to the Glossary screen to inform users that the glossary content needs to be downloaded. The text guides users on how to obtain the glossary if it doesn't download automatically.

--- app/src/main/assets/texts/glossary.md
+++ app/src/main/assets/texts/glossary.md
@@ -1,3 +1,8 @@
-# Glosario de preparados
 
-## Preparados medicinales
+# El glosario todavía no se ha descargado...
+
+En esta página podrás consultar la definición de varias palabras relevantes en la etnobotánica.
+
+Para ello, espera a que se descargue automáticamente de internet la definición de cada palabra.
+
+Cierra y abre la aplicación cuando tengas internet si no se te muestra automáticamente al cabo de un rato.
--- app/src/main/java/org/druidanet/druidnet/ui/screens/GlossaryScreen.kt
+++ app/src/main/java/org/druidanet/druidnet/ui/screens/GlossaryScreen.kt
@@ -1,24 +1,18 @@
 package org.druidanet.druidnet.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mikepenz.markdown.m3.Markdown
 import com.mikepenz.markdown.m3.markdownTypography
-import org.druidanet.druidnet.R
 import org.druidanet.druidnet.ui.theme.DruidNetTheme
 
 @Composable
