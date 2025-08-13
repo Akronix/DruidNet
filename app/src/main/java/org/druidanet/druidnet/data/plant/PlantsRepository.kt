d149b8096361d9492c854177435cedca9a1cd6b5
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2025-08-13 23:54:11 +0200

    Fix bug that breaks the app when there was no display name and latin name should be used when entering in the plant sheet screen.
    This commit updates the `PlantDAO` and `PlantsRepository` to correctly handle cases where a plant's display name might not be available in the requested language.
    
    Signed-off-by: Abel 'Akronix' Serrano Juste <akronix5@gmail.com>

--- app/src/main/java/org/druidanet/druidnet/data/plant/PlantsRepository.kt
+++ app/src/main/java/org/druidanet/druidnet/data/plant/PlantsRepository.kt
@@ -1,16 +1,12 @@
 package org.druidanet.druidnet.data.plant
 
-import org.druidanet.druidnet.model.LanguageEnum
-import org.druidanet.druidnet.model.Plant
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.last
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapNotNull
+import org.druidanet.druidnet.model.LanguageEnum
+import org.druidanet.druidnet.model.Plant
 import org.druidanet.druidnet.model.PlantCard
 import org.druidanet.druidnet.network.PlantDataDTO
 import org.druidanet.druidnet.ui.toPlant
@@ -31,7 +27,7 @@ class PlantsRepository(
         return plantDao.getPlant(plantLatinName)
             .filterNotNull()
             .map {
-                val displayName = plantDao.getDisplayName(it.p.plantId, language).first()
+                val displayName = plantDao.getDisplayName(it.p.plantId, language).firstOrNull() ?: plantLatinName
                 it.toPlant(displayName = displayName)
             }
     }
