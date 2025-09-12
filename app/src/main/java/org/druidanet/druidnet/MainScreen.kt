0d9a4b49764b734d8274e8377d12da61baa56018
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2025-09-12 13:10:22 +0200

    Upgrade hilt dep to 1.3.0
    Signed-off-by: Abel 'Akronix' Serrano Juste <akronix5@gmail.com>

--- app/src/main/java/org/druidanet/druidnet/MainScreen.kt
+++ app/src/main/java/org/druidanet/druidnet/MainScreen.kt
@@ -44,7 +44,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
--- app/src/main/java/org/druidanet/druidnet/ui/plant_sheet/PlantSheetScreen.kt
+++ app/src/main/java/org/druidanet/druidnet/ui/plant_sheet/PlantSheetScreen.kt
@@ -49,7 +49,7 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.mikepenz.markdown.m3.Markdown
 import com.mikepenz.markdown.m3.markdownColor
 import com.mikepenz.markdown.m3.markdownTypography
