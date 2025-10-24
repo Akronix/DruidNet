5e3582c46fe52bae23a2deb0a823aeb495eea81b
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2025-10-24 15:43:13 +0200

    Update Android UI and compose library versions
    Signed-off-by: Abel 'Akronix' Serrano Juste <akronix5@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -168,6 +168,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
