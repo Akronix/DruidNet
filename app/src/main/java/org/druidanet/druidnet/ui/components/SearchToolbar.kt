44da615805cf72d9cdb6085174d40082499c9ac4
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2026-02-01 20:36:54 +0100

    Make SearchToolbar a reusable component
    Signed-off-by: Abel 'Akronix' Serrano Juste <akronix5@gmail.com>

---
+++ app/src/main/java/org/druidanet/druidnet/ui/components/SearchToolbar.kt
@@ -0,0 +1,166 @@
+package org.druidanet.druidnet.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.druidanet.druidnet.R
+import org.druidanet.druidnet.ui.theme.DruidNetTheme
+
+@Composable
+fun SearchToolbar(
+    searchQuery: String,
+    placeholderSearchText: String,
+    onSearchQueryChanged: (String) -> Unit,
+    onSearchTriggered: (String) -> Unit,
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.background),
+    ) {
+        IconButton(onClick = { onBackClick() }) {
+            Icon(
+                painterResource(R.drawable.arrow_back),
+                contentDescription = stringResource(
+                    id = R.string.back_button,
+                ),
+            )
+        }
+        SearchTextField(
+            searchQuery = searchQuery,
+            placeholderText = placeholderSearchText,
+            onSearchQueryChanged = onSearchQueryChanged,
+            onSearchTriggered = onSearchTriggered,
+        )
+    }
+}
+
+@Composable
+private fun SearchTextField(
+    searchQuery: String,
+    placeholderText: String,
+    onSearchQueryChanged: (String) -> Unit,
+    onSearchTriggered: (String) -> Unit,
+) {
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val onSearchExplicitlyTriggered = {
+        keyboardController?.hide()
+        onSearchTriggered(searchQuery)
+    }
+
+    TextField(
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+        ),
+        placeholder = {Text(placeholderText)},
+        leadingIcon = {
+            Icon(
+                painterResource(R.drawable.search),
+                contentDescription = stringResource(
+                    id = R.string.feature_search_title,
+                ),
+                tint = MaterialTheme.colorScheme.onSurface,
+            )
+        },
+        trailingIcon = {
+            if (searchQuery.isNotEmpty()) {
+                IconButton(
+                    onClick = {
+                        onSearchQueryChanged("")
+                    },
+                ) {
+                    Icon(
+                        painterResource(R.drawable.close),
+                        contentDescription = stringResource(
+                            id = R.string.feature_search_clear_search_text_content_desc,
+                        ),
+                        tint = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
+            }
+        },
+        onValueChange = {
+            if ("\n" !in it) onSearchQueryChanged(it)
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+//            .padding(16.dp)
+            .padding(bottom = 6.dp, top = 6.dp, start = 10.dp, end = 16.dp)
+            .focusRequester(focusRequester)
+            .onKeyEvent {
+                if (it.key == Key.Enter) {
+                    if (searchQuery.isBlank()) return@onKeyEvent false
+                    onSearchExplicitlyTriggered()
+                    true
+                } else {
+                    false
+                }
+            }
+            .testTag("searchTextField"),
+        shape = RoundedCornerShape(32.dp),
+        value = searchQuery,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Search,
+        ),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+//                if (searchQuery.isBlank()) return@KeyboardActions
+                onSearchExplicitlyTriggered()
+            },
+        ),
+        maxLines = 1,
+        singleLine = true,
+    )
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+}
+
+@Preview
+@Composable
+private fun SearchToolbarPreview() {
+    DruidNetTheme {
+        SearchToolbar(
+            searchQuery = "",
+            placeholderSearchText = "Type your search query here",
+            onBackClick = {},
+            onSearchQueryChanged = {},
+            onSearchTriggered = {},
+        )
+    }
+}
--- app/src/main/java/org/druidanet/druidnet/ui/screens/CatalogScreen.kt
+++ app/src/main/java/org/druidanet/druidnet/ui/screens/CatalogScreen.kt
@@ -4,12 +4,10 @@ import android.annotation.SuppressLint
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.consumeWindowInsets
@@ -25,21 +23,13 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -47,22 +37,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.samples.apps.nowinandroid.core.designsystem.component.scrollbar.DraggableScrollbar
@@ -77,6 +57,7 @@ import org.druidanet.druidnet.navigation.CatalogDestination
 import org.druidanet.druidnet.ui.DruidNetViewModel
 import org.druidanet.druidnet.ui.theme.DruidNetTheme
 import org.druidanet.druidnet.utils.assetsToBitmap
+import org.druidanet.druidnet.ui.components.SearchToolbar
 
 @Composable
 fun PlantCard(
@@ -208,6 +189,7 @@ fun CatalogScreen(
                 } else {
                     SearchToolbar(
                         searchQuery = searchQuery,
+                        placeholderSearchText = stringResource(R.string.feature_search_textfield),
                         onSearchQueryChanged = { searchQuery = it },
                         onSearchTriggered = { searchQuery = it.trim() },
                         onBackClick = { isSearchBar = false; searchQuery = "" },
@@ -257,133 +239,6 @@ fun NoPlantsScreen(modifier: Modifier) {
                         "¿Echas algo de menos? [Envíanos un mensaje](mailto:druidnetbeta@gmail.com)"
             )
         }
-    }
-}
-
-@Composable
-private fun SearchToolbar(
-    searchQuery: String,
-    onSearchQueryChanged: (String) -> Unit,
-    onSearchTriggered: (String) -> Unit,
-    onBackClick: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .fillMaxWidth()
-            .background(color = MaterialTheme.colorScheme.background),
-    ) {
-        IconButton(onClick = { onBackClick() }) {
-            Icon(
-                painterResource(R.drawable.arrow_back),
-                contentDescription = stringResource(
-                    id = R.string.back_button,
-                ),
-            )
-        }
-        SearchTextField(
-            searchQuery = searchQuery,
-            onSearchQueryChanged = onSearchQueryChanged,
-            onSearchTriggered = onSearchTriggered,
-        )
-    }
-}
-
-@Composable
-private fun SearchTextField(
-    searchQuery: String,
-    onSearchQueryChanged: (String) -> Unit,
-    onSearchTriggered: (String) -> Unit,
-) {
-    val focusRequester = remember { FocusRequester() }
-    val keyboardController = LocalSoftwareKeyboardController.current
-
-    val onSearchExplicitlyTriggered = {
-        keyboardController?.hide()
-        onSearchTriggered(searchQuery)
-    }
-
-    TextField(
-        colors = TextFieldDefaults.colors(
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent,
-        ),
-        placeholder = {Text(stringResource(R.string.feature_search_textfield))},
-        leadingIcon = {
-            Icon(
-                painterResource(R.drawable.search),
-                contentDescription = stringResource(
-                    id = R.string.feature_search_title,
-                ),
-                tint = MaterialTheme.colorScheme.onSurface,
-            )
-        },
-        trailingIcon = {
-            if (searchQuery.isNotEmpty()) {
-                IconButton(
-                    onClick = {
-                        onSearchQueryChanged("")
-                    },
-                ) {
-                    Icon(
-                        painterResource(R.drawable.close),
-                        contentDescription = stringResource(
-                            id = R.string.feature_search_clear_search_text_content_desc,
-                        ),
-                        tint = MaterialTheme.colorScheme.onSurface,
-                    )
-                }
-            }
-        },
-        onValueChange = {
-            if ("\n" !in it) onSearchQueryChanged(it)
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-//            .padding(16.dp)
-            .padding(bottom = 6.dp, top = 6.dp, start = 10.dp, end = 16.dp)
-            .focusRequester(focusRequester)
-            .onKeyEvent {
-                if (it.key == Key.Enter) {
-                    if (searchQuery.isBlank()) return@onKeyEvent false
-                    onSearchExplicitlyTriggered()
-                    true
-                } else {
-                    false
-                }
-            }
-            .testTag("searchTextField"),
-        shape = RoundedCornerShape(32.dp),
-        value = searchQuery,
-        keyboardOptions = KeyboardOptions(
-            imeAction = ImeAction.Search,
-        ),
-        keyboardActions = KeyboardActions(
-            onSearch = {
-//                if (searchQuery.isBlank()) return@KeyboardActions
-                onSearchExplicitlyTriggered()
-            },
-        ),
-        maxLines = 1,
-        singleLine = true,
-    )
-    LaunchedEffect(Unit) {
-        focusRequester.requestFocus()
-    }
-}
-
-@Preview
-@Composable
-private fun SearchToolbarPreview() {
-    DruidNetTheme {
-        SearchToolbar(
-            searchQuery = "",
-            onBackClick = {},
-            onSearchQueryChanged = {},
-            onSearchTriggered = {},
-        )
     }
 }
 
