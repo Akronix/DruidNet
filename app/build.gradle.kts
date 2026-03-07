8ccfef63a7c4cd96bce20bc9886ce10d3b66b069
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2026-03-07 21:46:59 +0100

    Release version 1.7.1
    Signed-off-by: Abel 'Akronix' Serrano Juste <akronix5@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "org.druidanet.druidnet"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.7.0"
+        versionCode = 25
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
