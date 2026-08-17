9f5e48a7c26fcdb52d86d74e81f90edebc9ac976
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2026-08-17 17:09:23 +0200

    Show 8 extra online images instead of just 5
    Signed-off-by: Abel 'Akronix' Serrano Juste <akronix5@gmail.com>

--- app/src/main/java/org/druidanet/druidnet/network/iNaturalistApiService.kt
+++ app/src/main/java/org/druidanet/druidnet/network/iNaturalistApiService.kt
@@ -11,7 +11,7 @@ interface iNaturalistApiService {
     @GET("taxa?iconic_taxa=Plantae")
     suspend fun retrieveTaxaRecord(@Query("q") q: String, @Query("rank") rank: String = "species"): Response<JsonObject>
 
-    @GET("observations?photos=true&iconic_taxa=Plantae&quality_grade=research&per_page=5&order=desc&order_by=geo_score&only_id=false")
+    @GET("observations?photos=true&iconic_taxa=Plantae&quality_grade=research&per_page=8&order=desc&order_by=geo_score&only_id=false")
     suspend fun retrieveImages(@Query("taxon_id") taxonId: Int): Response<JsonObject>
 
 }
